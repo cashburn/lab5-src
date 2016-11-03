9c2b638728b27d8dae133e021e94ac08be00bb19
Colin Ashburn <cashburn@purdue.edu>
2016-11-03 13:57:50 -0400

    Commit Web Server

--- myhttpd.cpp
+++ myhttpd.cpp
@@ -1,7 +1,20 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
 
 int main( int argc, char **argv) {
-
-
-
+	int c;
+	FILE * fp = fopen("http-root-dir/htdocs/index.html", "r");
+	if (fp) {
+		while ((c = getc(fp)) != EOF)
+			putchar(c);
+		fclose(fp);
+	}
 }
 
