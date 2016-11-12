b7383ab4ca21f509455ba01583ca35796523bc16
Colin Ashburn <cashburn@purdue.edu>
2016-11-12 13:49:15 -0500

    Commit Web Server

--- myhttpd.cpp
+++ myhttpd.cpp
@@ -148,6 +148,7 @@ int main( int argc, char ** argv ) {
 			pthread_attr_t attr;
 			pthread_attr_init(&attr);
 			pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
+			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
 			pthread_create(&t, &attr,
 				(void * (*)(void *)) processRequestThread, (void *) (intptr_t) slaveSocket);
 		}
