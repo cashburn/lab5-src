d78a77f9522d31ad7ebb6c72d63334797667b86e
Colin Ashburn <cashburn@purdue.edu>
2016-11-13 18:09:28 -0500

    Commit Web Server

--- myhttpd.cpp
+++ myhttpd.cpp
@@ -335,7 +335,7 @@ void poolSlave(void * masterSocket) {
 		}
 		processRequest((intptr_t) slaveSocket);
 		printf("close socket\n");
-		shutdown((intptr_t) socket, SHUT_RDWR);
+		shutdown((intptr_t) socket, 1);
 		close((intptr_t) socket);
 	}
 }
